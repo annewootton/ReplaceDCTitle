46e97c95e7ac8e50895330eebbc61f026773cf89
Anne Wootton <anne.wootton@gmail.com>
2012-11-09 10:40:18 -0800

    Replacing files for sync.

---
+++ plugin.php
@@ -0,0 +1,17 @@
+<?php
+
+function item_title_switch($title, $item) {
+/**
+ * Replace the title of all items in public/admin view with a different field.
+ *
+ * For use with element sets other than PBCore, change the new field to whatever field you want, and activate the plugin.
+ */
+    $request = Zend_Controller_Front::getInstance()->getRequest();
+
+        // Replace title field here.
+        $title = item('PBCore', 'Title', null, $item);
+
+    return $title;
+}
+
+add_filter(array('Display', 'Item', 'Dublin Core', 'Title'), item_title_switch);
